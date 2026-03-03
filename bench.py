f5f934cf98a13bb21da668371c40be1fea3538de
Logan Bolton <loganbolton101@gmail.com>
2026-03-02 23:34:19 -0600

    test

--- bench.py
+++ bench.py
@@ -3,8 +3,10 @@ A much shorter version of train.py for benchmarking
 """
 import os
 from contextlib import nullcontext
+
 import numpy as np
-import time
+  import time
+
 import torch
 from model import GPTConfig, GPT
 
